e376b2b262e28fce44c3abd6096645f53115b6df
DYM-AUTO <56385391+DYM-AUTO@users.noreply.github.com>
2019-10-10 22:09:41 +0700

    RMVK+BUZZER
    Добавлено управление блоком РММК+пищалка DYM-AUTO

--- adc.h
+++ adc.h
@@ -1,7 +1,7 @@
 //   Проект центра открытого проектирования у Счастливчика https://LuckyCenter.ru
 #ifndef _ADC_h
 #define _ADC_h
-
+//*****
 #if defined(ARDUINO) && ARDUINO >= 100
 	#include "arduino.h"
 #else
